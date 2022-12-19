6fc808776c15f32121ee84e642a5ee19f7ae15dd
Eman Abdallah <115192151+Emanabdallah92@users.noreply.github.com>
2022-12-20 01:08:58 +0200

    Update main.c
    style: Formatting
    Remove unuseful comments

--- luhn function/main.c
+++ luhn function/main.c
@@ -7,12 +7,10 @@ int main()
     printf("Enter the primary account number: ");
     scanf(" %[^\n]",pan_num);
     int pan_num_len= strlen(pan_num);
-    //printf("%d\n", pan_num_len);
+   
     long long pan;
     sscanf(pan_num, " %llu", &pan);
-    //printf("%llu\n", pan);
-    //if(pan==0)
-        //return WRONG_PAN;
+   
         long long q=pan;
     int i, rem, sum=0;
     for(i=0; i<pan_num_len; i++)
@@ -26,7 +24,7 @@ int main()
         q=q/10;
     }
     printf("%d\n",sum);
-    //bool Luhn_check=0;
+    
     if((sum%10)!=0)
         printf("Not A Luhn Number");
         else
